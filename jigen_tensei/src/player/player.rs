ad03d522c813b1b5dc35032b89d156f37e7b6195
Bratah123 <brandonnguyen3301@gmail.com>
2023-05-20 14:38:44 -0700

    FEAT: Add left & right movements

--- jigen_tensei/src/player/player.rs
+++ jigen_tensei/src/player/player.rs
@@ -6,10 +6,12 @@ struct Player;
 
 pub struct PlayerPlugin;
 
+pub const PLAYER_SPEED: f32 = 350.0;
+
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(startup_system)
-            .add_system(print_cube_altitude_system);
+            .add_system(player_movement_system);
     }
 }
 
@@ -17,15 +19,35 @@ fn startup_system(mut commands: Commands) {
     // Spawn Main Character
     /* Create our temporary cube player. */
     commands
-        .spawn(RigidBody::Dynamic)
+        .spawn(RigidBody::KinematicPositionBased)
         .insert(Collider::cuboid(50 as Real, 50 as Real))
-        .insert(Restitution::coefficient(0.7))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)))
+        .insert(TransformBundle::from(Transform::from_xyz(200.0, 0.0, 0.0)))
+        .insert(GravityScale(1.0))
+        .insert(KinematicCharacterController::default())
         .insert(Player);
+
+    // Spawn a ball into the testing world
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 100.0, 0.0)));
 }
 
-fn print_cube_altitude_system(positions: Query<&Transform, With<RigidBody>>) {
-    for transform in positions.iter() {
-        println!("cube altitude: {}", transform.translation.y);
+fn player_movement_system(
+    keys: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut KinematicCharacterController, With<Player>>,
+    time: Res<Time>) {
+    if let Ok(mut controller) = player_query.get_single_mut() {
+        let mut direction = Vec2::ZERO;
+        if keys.pressed(KeyCode::D) {
+            direction.x += 1.0;
+        }
+        else if keys.pressed(KeyCode::A) {
+            direction.x -= 1.0;
+        }
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+        controller.translation = Some(direction * PLAYER_SPEED * time.delta_seconds());
     }
 }
