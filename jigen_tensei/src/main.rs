ecf06803abd27b33098c4a7f43493667c32f7fdf
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2023-05-20 15:53:58 +0800

    FEAT: Add logger

--- jigen_tensei/src/main.rs
+++ jigen_tensei/src/main.rs
@@ -1,15 +1,44 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]  // no external terminal window for release
+use bevy::log::LogPlugin;
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugin(LoggerPlugin)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup_graphics)
         .add_startup_system(setup_physics)
         .add_system(print_ball_altitude)
         .run();
+
+
+}
+
+/// Contains default plugins with tweaks for the logger
+///
+/// If running in debug mode, warn level is at DEBUG.
+/// Release builds have the warn level set to INFO.
+struct LoggerPlugin;
+impl Plugin for LoggerPlugin {
+    fn build(&self, app: &mut App) {
+        // Debug mode
+        #[cfg(debug_assertions)]
+        app.add_plugins(DefaultPlugins.set(LogPlugin {
+            level: bevy::log::Level::DEBUG,
+            filter: "debug,wgpu_core=warn,wgpu_hal=warn,jigen_tensei=debug".into(),
+        }));
+
+        // Release mode
+        #[cfg(not(debug_assertions))]
+        app.add_plugins(DefaultPlugins.set(LogPlugin {
+            level: bevy::log::Level::INFO,
+            filter: "info,wgpu_core=warn,wgpu_hal=warn".into(),
+        }));
+    }
+    // Example log line: debug!("x: {}, state: {:?}", x, state);
+    // Example log line: error!("Unknown condition!");
 }
 
 fn setup_graphics(mut commands: Commands) {
@@ -36,6 +65,6 @@ fn setup_physics(mut commands: Commands) {
 
 fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
     for transform in positions.iter() {
-        println!("Ball altitude: {}", transform.translation.y);
+        debug!("Ball altitude: {}", transform.translation.y);
     }
 }
