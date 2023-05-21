4fe87e6103ab33a2c7f9846dea50b4cb2766c625
Bratah123 <brandonnguyen3301@gmail.com>
2023-05-20 17:51:26 -0700

    FEAT: Add dynamic rigidbody to character

--- jigen_tensei/src/player/player.rs
+++ jigen_tensei/src/player/player.rs
@@ -7,12 +7,13 @@ pub struct Player;
 pub struct PlayerPlugin;
 
 pub const PLAYER_SPEED: f32 = 350.0;
+pub const JUMP_VEL: f32 = 65000.0;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(startup_system)
             .add_system(player_movement_system)
-            .add_system(apply_player_gravity_system);
+            .add_system(read_result_system);
     }
 }
 
@@ -20,11 +21,16 @@ fn startup_system(mut commands: Commands) {
     // Spawn Main Character
     /* Create our temporary cube player. */
     commands
-        .spawn(RigidBody::KinematicPositionBased)
-        .insert(Collider::cuboid(50 as Real, 50 as Real))
-        .insert(TransformBundle::from(Transform::from_xyz(200.0, 0.0, 0.0)))
-        .insert(GravityScale(1.0))
+        .spawn(RigidBody::Dynamic)
         .insert(KinematicCharacterController::default())
+        .insert(Velocity {
+            linvel: Vec2::new(0.0, 0.0),
+            angvel: 0.,
+        })
+        .insert(LockedAxes::ROTATION_LOCKED)
+        .insert(GravityScale(12.0))
+        .insert(Collider::cuboid(50 as Real, 50 as Real))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 300.0, 0.0)))
         .insert(Player);
 
     // Spawn a ball into the testing world
@@ -36,42 +42,29 @@ fn startup_system(mut commands: Commands) {
 
 fn player_movement_system(
     keys: Res<Input<KeyCode>>,
-    mut player_query: Query<&mut KinematicCharacterController, With<Player>>,
-    time: Res<Time>,
-) {
-    if let Ok(mut controller) = player_query.get_single_mut() {
+    mut player_query: Query<(&mut Velocity, &mut KinematicCharacterController), With<Player>>,
+    time: Res<Time>) {
+    for (mut velocity, mut controller) in player_query.iter_mut() {
         let mut direction = Vec2::ZERO;
+        let mut pressed = false;
         if keys.pressed(KeyCode::D) {
-            direction.x += 1.0;
-        } else if keys.pressed(KeyCode::A) {
-            direction.x -= 1.0;
+            direction = Vec2::new(1.0, 0.0);
+            pressed = true;
         }
-        if direction.length() > 0.0 {
-            direction = direction.normalize();
+        else if keys.pressed(KeyCode::A) {
+            direction = Vec2::new(-1.0, 0.0);
+            pressed = true;
         }
-        controller.translation = Some(direction * PLAYER_SPEED * time.delta_seconds());
+        if keys.pressed(KeyCode::Space) {
+            velocity.linvel = Vec2::new(0., 1.0) * JUMP_VEL * time.delta_seconds();
+        }
+        if pressed {
+            controller.translation = Some(direction * PLAYER_SPEED * time.delta_seconds());
+        }
     }
 }
-
-fn apply_player_gravity_system(
-    mut player_query: Query<
-        (
-            &mut KinematicCharacterController,
-            &KinematicCharacterControllerOutput,
-        ),
-        With<Player>,
-    >,
-    time: Res<Time>,
-) {
-    for (mut controller, output) in player_query.iter_mut() {
-        if !output.grounded {
-            controller.translation = match controller.translation {
-                Some(mut v) => {
-                    v.y = -1.0;
-                    Some(v)
-                }
-                None => Some(Vec2::new(0.0, -1.0) * PLAYER_SPEED * time.delta_seconds()),
-            };
-        }
+fn read_result_system(controllers: Query<&KinematicCharacterControllerOutput>) {
+    for controller in controllers.iter() {
+        println!("Grounded: {:?}", controller.grounded);
     }
 }
